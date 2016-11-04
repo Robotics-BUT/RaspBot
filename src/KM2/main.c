c051123280a65044650af9b43b38d2e091fa788b
Frantisek Burian <BuFran@seznam.cz>
2016-11-04 17:55:36 +0100

    KM2: novy firmware mnoho funkci navic:
     * funkcni obousmerna komunikace po I2C,
     * diferencialni odometrie,
     * parametrizace regulatoru pres i2c registry,
     * nastaveni hodnot pri bootu pres eeprom,
     * automaticke zastaveni bez prijimanych dat v casovem okne
     * automaticke odpojeni proudu do motoru pri delsi dobe necinnosti robotu (ochrana proti tepelnemu namahani)
    
    Na RaspberryPi je nutne zpomalit I2C na max 32kHz, kvuli ClockStretching ktere AVR ma ale Raspberry nema. (jinak dochazi k poskozeni dat)
    
    Nastaveni SPEED by melo odpovidat rychlosti cca (1220 Hz * SPEED / 80) pulsu/s. Pri nastaveni rychlosti 10 da odometrie za sekundu 156 pulsu.
    
    Pri MAXSPEED 8192 (default) je posilan v kazdem taktu trs 102 impulsu, pri mikrokroku 32 to dela 3 kroky (5.8 stupnu) kazdou ms tj 15 otacek za sekundu.
    To motor pod zatizenim rozhodne neda. Do budoucna se nejspise hne nahoru s konstantou 80 aby prevodni funkce SPEED mela rozumne limity a bylo dosazitelne
    vysoke rozliseni rychlosti.

--- src/KM2/main.c
+++ src/KM2/main.c
@@ -24,49 +24,167 @@
 #include <avr/eeprom.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
+#include <util/twi.h>
 #include <stdint.h>
 #include "board.h"
 #include "gpio.h"
 #include "motors.h"
-#include "i2c_slave.h"
 
 
-uint8_t twibuf[64];
+struct __attribute__((packed)) {
+	uint8_t data[0];	
+	uint8_t addr;
+	union {
+		struct {
+			uint16_t a;
+			uint16_t b;
+			uint16_t c;
+			uint16_t d;
+		} u16;
+
+		struct {
+			uint32_t a;
+			uint32_t b;
+			uint32_t c;
+			uint32_t d;
+		} u32;
+	} u;
+	
+} buffer;
+
+
 uint8_t twibufptr = 0;
 uint8_t twibufptrlen = 0;
 
 uint8_t EEMEM  _cfg_i2c_address = 0xE0;
+uint8_t EEMEM  _cfg_i2c_mask = 0xFF;
 uint16_t EEMEM  _cfg_motor_maxspeed = 8192;
+uint16_t EEMEM  _cfg_motor_timeout = 1220;				// ~1 sec
+uint16_t EEMEM  _cfg_motor_timeout_pwoff = 1220 * 5;	// ~5 sec
+
+
+#define KM2_SPEED             0x00	  // 2x16bit signed
+#define KM2_ODOMETRY          0x01    // 2x32bit signed
+#define KM2_STATUS            0x02	  // 16bit unsigned
+
+#define KM2_CMDRESET		  0xE0	  // no data
+#define KM2_CMDLOAD			  0xE1	  // no data
+#define KM2_CMDSTORE		  0xE2	  // no data
+
+
+#define KM2_CFGMAXSPD         0xF0    // 16bit unsigned
+#define KM2_CFGTIMEOUT        0xF1    // 16bit unsigned
+#define KM2_CFGTIMEOUTPWOFF   0xF2    // 16bit unsigned
+
+#define KM2_CFGADDR           0xFF    // 8bit unsigned [or 16bit unsigned with mask]
+
 
 static inline uint8_t i2c_initread()
 {
-	return 4;
-}
+	switch (buffer.addr) {
+	case KM2_SPEED:
+		// TODO read actual speeds
+		buffer.u.u16.a = 0x0102;
+		buffer.u.u16.b = 0x0304;
+		return 5;
 
-static inline uint8_t i2c_initwrite()
-{
-	return 5;
-}
+	case KM2_ODOMETRY:
+		cli();
+		buffer.u.u32.a = motor1_odometry;
+		buffer.u.u32.b = motor2_odometry;
+		motor1_odometry = 0;
+		motor2_odometry = 0;
+		sei();
+		return 9;
 
-char twia = 0;
+	case KM2_STATUS:
+		// TODO read actual status of drive
+		buffer.u.u16.a = 0x0102;
+		return 3;
+
+	case KM2_CFGMAXSPD:
+		buffer.u.u16.a = cfg_motor_maxspeed;
+		return 3;
+
+	case KM2_CFGTIMEOUT:
+		buffer.u.u16.a = cfg_motor_timeout;
+		return 3;
+
+	case KM2_CFGTIMEOUTPWOFF:
+		buffer.u.u16.a = cfg_motor_timeout_pwoff;
+		return 3;
+	}
+	return 0;
+}
 
 static inline void i2c_finishwrite(uint8_t cnt)
 {
 	if (cnt < 1)
 		return;
 	
-	twia = twibuf[0];
-
-	if (cnt < 3)
+	switch (buffer.addr) {
+	case KM2_SPEED:
+		if (cnt == 5)
+			motor_set(buffer.u.u16.a, buffer.u.u16.b);
 		return;
-	
-	switch (twia) {
-	case 0:
-		if (cnt < 5)
-			return;
 
-		motor_set((int16_t)(twibuf[2] << 8) | twibuf[1], (int16_t)(twibuf[4] << 8) | twibuf[3]);
-		break;
+	/************************************************************************/
+	/* Commands                                                             */
+	/************************************************************************/
+	case KM2_CMDRESET:
+		wdt_enable(WDTO_60MS);
+		for(;;);
+		return;// never goes here
+
+	case KM2_CMDLOAD:
+		wdt_reset();
+		cfg_motor_maxspeed = eeprom_read_word(&_cfg_motor_maxspeed);
+		cfg_motor_timeout = eeprom_read_word(&_cfg_motor_timeout);
+		cfg_motor_timeout_pwoff = eeprom_read_word(&_cfg_motor_timeout_pwoff);
+		return;
+
+	case KM2_CMDSTORE:
+		wdt_reset();
+		eeprom_update_word(&_cfg_motor_maxspeed, cfg_motor_maxspeed);
+		eeprom_update_word(&_cfg_motor_timeout, cfg_motor_timeout);
+		eeprom_update_word(&_cfg_motor_timeout_pwoff, cfg_motor_timeout_pwoff);
+		return;
+		
+	/************************************************************************/
+	/* Configuration                                                        */
+	/************************************************************************/
+	case KM2_CFGMAXSPD:
+		if (cnt == 3) 
+			cfg_motor_maxspeed = buffer.u.u16.a;
+		return;
+
+	case KM2_CFGTIMEOUT:
+		if (cnt == 3) 
+			cfg_motor_timeout = buffer.u.u16.a;
+		return;
+
+	case KM2_CFGTIMEOUTPWOFF:
+		if (cnt == 3)
+			cfg_motor_timeout_pwoff = buffer.u.u16.a;
+		return;
+
+	case KM2_CFGADDR:
+		if (cnt == 2) {
+			wdt_enable(WDTO_1S);
+			wdt_reset();
+			eeprom_update_byte(&_cfg_i2c_address, buffer.data[1]);
+			eeprom_update_byte(&_cfg_i2c_mask, 0xFF);
+			wdt_enable(WDTO_60MS);
+			for(;;);
+		} else if (cnt == 3) {
+			wdt_enable(WDTO_1S);
+			wdt_reset();
+			eeprom_update_byte(&_cfg_i2c_address, buffer.data[1]);
+			eeprom_update_byte(&_cfg_i2c_mask, buffer.data[2]);
+			wdt_enable(WDTO_60MS);
+			for(;;);
+		}		
+		return;
 	}
 	
 	return;
@@ -74,12 +192,20 @@ static inline void i2c_finishwrite(uint8_t cnt)
 
 int main(void)
 {
+	wdt_reset();
+	wdt_enable(WDTO_1S);
+
 	// initialize peripherals
 	cfg_motor_maxspeed = eeprom_read_word(&_cfg_motor_maxspeed);
+	cfg_motor_timeout = eeprom_read_word(&_cfg_motor_timeout);
+	cfg_motor_timeout_pwoff = eeprom_read_word(&_cfg_motor_timeout_pwoff);
+
+	TWCR = 0;				
+	TWAR = eeprom_read_byte(&_cfg_i2c_address);
+	TWAMR = ~eeprom_read_byte(&_cfg_i2c_mask);
+	TWCR = _BV(TWEN) | _BV(TWEA);
 
 	motor_prepare();
-
-	i2c_slave_init(eeprom_read_byte(&_cfg_i2c_address),0);
 
 	motor_set(0, 0);    // 4000 max s rampou
 	
@@ -88,32 +214,34 @@ int main(void)
 	for(;;) {
 		wdt_reset();
 		if (TWCR & _BV(TWINT)) {
-			switch ((uint8_t)((TWSR >> 3) & 0x1F)){
+			switch (TW_STATUS) {
 			// Transmitter
-			case TWI_ST_SLA_ACK >> 3:
+			case TW_ST_SLA_ACK:
 				twibufptrlen = i2c_initread();
-				twibufptr = 0;
+				twibufptr = 1;
 		
-			case TWI_ST_DATA_ACK >> 3:
-				TWDR = (twibufptr >= twibufptrlen) ? 0 : twibuf[twibufptr++];
-				break;
+			case TW_ST_DATA_ACK:
+				TWDR = (twibufptr >= twibufptrlen) ? 0 : buffer.data[twibufptr++];
+				TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
+				continue;
 			
 			// Receiver
-			case TWI_SR_SLA_ACK >> 3:
-				twibufptrlen = i2c_initwrite();
+			case TW_SR_SLA_ACK:
 				twibufptr = 0;
 				break;
 			
-			case TWI_SR_DATA_ACK >> 3:
-				if (twibufptr < twibufptrlen)
-					twibuf[twibufptr++] = TWDR;
-				break;
+			case TW_SR_DATA_ACK:
+				if (twibufptr < sizeof(buffer))
+					buffer.data[twibufptr++] = TWDR;	// read data
+
+				// send ACK only if we still have buffer space for it
+				TWCR = _BV(TWEN) | _BV(TWINT) | ((twibufptr < (sizeof(buffer) - 1)) ? _BV(TWEA) : 0);
+				continue;
 			
-			case TWI_SR_STOP >> 3:
+			case TW_SR_STOP:
 				// fast forward because write can be longer
 				TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA);
-				i2c_finishwrite(twibufptr);
-				
+				i2c_finishwrite(twibufptr);				
 				continue;
 				
 			default:
